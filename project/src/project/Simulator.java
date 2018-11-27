d77138863e876ca9531ec683853da9486ad14150
tmdgh990201 <45379135+tmdgh990201@users.noreply.github.com>
2018-11-28 02:30:01 +0900

    2018-11-28-2

--- project/src/project/Simulator.java
+++ project/src/project/Simulator.java
@@ -3,27 +3,21 @@ package project;
 import java.awt.*;
 import javax.swing.*;
 
-public class Simulator {
+class Simulator extends JPanel {
+	public void paint(Graphics g) {
+		g.drawRect(10, 10, 300, 300);
+	}
 	public static void main(String[] args) {
-		JFrame frame = new JFrame("Simulator");
-		frame.setBounds(0, 0, 320, 420);
-		frame.setLayout(new BorderLayout());     
-		frame.setDefaultCloseOperation(frame.DISPOSE_ON_CLOSE);
+		JFrame frame = new JFrame("simulator");
+		Container c = frame.getContentPane();
 		
-		// 패널1 생성
-		JPanel panel1 = new JPanel();
-		panel1.setPreferredSize(new Dimension(320, 310));
-		panel1.setLayout(new FlowLayout());
-
-		// 패널2 생성
-		JPanel panel2 = new JPanel();
-		panel2.setLayout(new GridLayout(1, 3));
-		panel2.add(new JButton("LEFT"));
-		panel2.add(new JButton("FORWARD"));
-		panel2.add(new JButton("RIGHT"));
-		// 프레임에 패널 얹기
-		frame.add(panel1, BorderLayout.NORTH);
-		frame.add(panel2, BorderLayout.CENTER);
+		Simulator simulator = new Simulator();
+		c.add(simulator, BorderLayout.CENTER);
+		
+		frame.setLocation(0, 0);
+		frame.setPreferredSize(new Dimension(335, 460));
+		frame.pack();
 		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
